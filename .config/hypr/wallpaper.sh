fcd975484e3158173d12b28efba11860c3e7005e
adrien <adrien.bouvais.pro@gmail.com>
2025-05-02 16:49:05 +0200

    Removed cava stuff

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -14,10 +14,6 @@ main() {
     pywalfox update
     color1=$(awk 'match($0, /color2=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh)
     color2=$(awk 'match($0, /color3=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh)
-    cava_config="$HOME/.config/cava/config"
-    sed -i "s/^gradient_color_1 = .*/gradient_color_1 = '$color1'/" $cava_config
-    sed -i "s/^gradient_color_2 = .*/gradient_color_2 = '$color2'/" $cava_config
-    pkill -USR2 cava 2>/dev/null
     source ~/.cache/wal/colors.sh && cp -r $wallpaper ~/wallpapers/pywallpaper.jpg 
 }
 main
