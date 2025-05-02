722eabdb521bedb1b030a5b914e97d9fcbdaa247
adrien <adrien.bouvais.pro@gmail.com>
2025-05-02 16:51:58 +0200

    Removed auto SSH (not working) and added auto firest default wallpaper

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Install pacman packages
 for package in \
   scrcpy \
-  git \
   base-devel \
   lazygit \
   starship \
@@ -37,21 +36,12 @@ for package in \
   yazi \
   discord \
   firefox \
-  # blueman \
-  # bluez \
   ; do
     yay -S --noconfirm $package
     clear
 done
 
-# systemctl enable bluetooth
-
 # Install Configs
-mkdir ~/wallpapers
-sudo cp -rf ~/Dotfiles/wallpapers/* ~/wallpapers/
-wal -i ~/wallpapers/pywallpaper.jpg
-pywalfox install
-
 sudo cp -rf ~/Dotfiles/.config/* ~/.config/
 sudo cp ~/Dotfiles/.bashrc ~/.bashrc
 
@@ -59,9 +49,18 @@ git config --global user.email "adrien.bouvais.pro@gmail.com"
 git config --global user.name "adrien"
 ssh-keygen -t rsa -b 4096 -C "adrien.bouvais.pro@gmail.com" -f ~/.ssh/id_rsa -N ""
 
-cp /run/media/adrien/ARCH_202504/ssh_keys/id_rsa ~/.ssh/
-cp /run/media/adrien/ARCH_202504/ssh_keys/id_rsa.pub ~/.ssh/
+# Wallpaper
+mkdir ~/wallpapers
+sudo cp -rf ~/Dotfiles/wallpapers/* ~/wallpapers/
+wal -i ~/wallpapers/pywallpaper.jpg
+pywalfox install
 
-hyperctl reload
+swww img ~/.config/pywallpaper.jpg --transition-type any --transition-fps 60 --transition-duration .5
+wal -i ~/.config/pywallpaper.jpg -n --cols16
+swaync-client --reload-css
+cat ~/.cache/wal/colors-kitty.conf > ~/.config/kitty/current-theme.conf
+pywalfox update
+hyprctl reload
+
 echo "Dont forget to add Pywalfox extension to Firefox https://addons.mozilla.org/en-US/firefox/addon/pywalfox/"
 echo "Then run 'pywalfox update'"
